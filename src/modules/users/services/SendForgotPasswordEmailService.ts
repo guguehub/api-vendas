e1d41de9de86fcc7b6bb7a64200c71b9737a261a
guzz <dekmis@hotmail.com>
2024-01-22 15:40:20 -0300

    fix:(user token repository, send forgot password) adjusted to template use

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -19,12 +19,23 @@ class SendForgotPasswordEmailService {
       throw new AppError('user does not exist');
     }
 
-    const token = await userTokensRepository.generate(user.id);
+    const { token } = await userTokensRepository.generate(user.id);
     console.log(token);
 
     await EtherealMail.sendMail({
-      to: email,
-      body: `Solicitaçao redefiniçao de senha recebida: ${token?.token}`,
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de senha',
+
+      templateData: {
+        template: `Olá {{name}}: {{token}}`,
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
     });
   }
 }
