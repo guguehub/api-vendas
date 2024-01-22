f5f5b4e00bda96d20bdce6a4fb6c6a66b2994d88
guzz <dekmis@hotmail.com>
2024-01-22 16:25:11 -0300

    feat(email reset password complete) token via email

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,6 @@
 import { getCustomRepository } from 'typeorm';
 import AppError from '@shared/errors/AppError';
+import path from 'path';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
 import EtherealMail from '@config/mail/EtherealMail';
@@ -20,7 +21,14 @@ class SendForgotPasswordEmailService {
     }
 
     const { token } = await userTokensRepository.generate(user.id);
-    console.log(token);
+    //console.log(token);
+
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
 
     await EtherealMail.sendMail({
       to: {
@@ -30,10 +38,10 @@ class SendForgotPasswordEmailService {
       subject: '[API Vendas] Recuperação de senha',
 
       templateData: {
-        template: `Olá {{name}}: {{token}}`,
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });
